36347ca9971cb19a84a7595b023d9cb330e3a9ef
Pedram Zadno Azizi <pedram.azizi@gmail.com>
2017-06-11 22:44:04 +1000

    This is only for test.

---
+++ General Solution/WiseTech Global/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiseTech_Global
+{
+    public class Class1
+    {
+    }
+}
